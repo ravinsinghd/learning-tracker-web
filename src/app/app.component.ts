6e7c26af42dcb78dbfd4e761f8d007d0d93bca88
Ravin Singh D <ravinsinghd@outlook.com>
2021-05-15 16:15:49 +0530

    Added highchart & service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'learning-tracker-web';
+
+  constructor(private appService: AppService) {}
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppService {
+  constructor(private http: HttpClient) {}
+
+  getStatCounts() {
+    const url = `${environment.apiUrl}tracker/count`;
+    return this.httpGet(url);
+  }
+
+  httpGet<T>(url: string) {
+    return this.http.get<T>(url);
+  }
+}
